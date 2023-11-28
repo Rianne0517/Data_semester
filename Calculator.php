cc45272bebe43d20ed83a174327ceb014bfa898b
Rianne van der Meer <rianne.23@live.com>
2023-11-28 15:01:51 +0100

    CSV connectie met calculator

--- Calculator.php
+++ Calculator.php
@@ -1,63 +1,49 @@
 <?php 
-//Opent csv bestand
-//$file = fopen("bestandnaam.csv", "r");
-//while (! feof($file))
-//{
-    //print_r(fgetcsv($file));
-//}
 extract($_POST);
 if(isset($save))
 {
-    //$gewichtcoureur;
-    //$gewichtauto;
-
-    //de versnelling door zwaartekracht in meter per seconde in het kwadraat
-    $g= (9.81);
-    //de bochtradius in meters 
-    $r=(225);
-    //de wrijvingscoëfficiënt tussen de banden en het wegdek, de aanname is dat het voor alle f1 auto's hetzelfde is
-    $mu= (1.5);
-              
-    //Berekening max snelheid in een bocht 
-    $resultaat= ($mu * $g * $r * $bocht_nr);
-    $max= sqrt($resultaat);
-    $kmh= ($max * 3.6);
-    $max_snelheid= round($kmh);      
-
-}
-//Sluit het xsv bestand
-//fclose($file)
+    //$row = 1;
+    //Opent csv bestand
+    if (($file = fopen("database.csv", "r")) !== FALSE) 
+    {
+        while (($data = fgetcsv($file, 1000, ";")) !== FALSE) 
+        {
+            if ($data[1] == $bocht_nr)
+            {
+                $r = $data[3];
+                break;
+            }                    
+        }   
+        //de versnelling door zwaartekracht in meter per seconde in het kwadraat
+        $g= (9.81);               
+        //de wrijvingscoëfficiënt tussen de banden en het wegdek, de aanname is dat het voor alle f1 auto's hetzelfde is
+        $mu= (1.5);                
+        //Berekening max snelheid in een bocht 
+        $resultaat= ($mu * $g * $r);
+        $max= sqrt($resultaat);
+        $kmh= ($max * 3.6);
+        //rond het getal af op hele kilometers per uur 
+        $max_snelheid= round($kmh);    
+    }
+    //Sluit csv bestand
+    fclose($file);
+}  
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 </head>
-
 <body>
     <h1>Calculator</h1>
-    <form method="post">
+    <form method="post" action="Calculator.php">
         <table>
-            <tr>
-                <div>
-                    <!--<label>Gewicht coureur in kilogrammen </label>
-                    <input type="number" name="" class="form-control" placeholder="" value="<?php  echo @$gewichtcoureur;?>">-->
-                </div>
-            </tr>
-            <tr>
-                <div>
-                    <!--<label>Gewicht auto in kilogrammen</label>
-                    <input type="number" name="" class="form-control" placeholder="" value="<?php  echo isset($gewichtauto)?>"> -->
-                </div>
-            </tr>
             <tr>
                 <div class="form-group row input-group">
                     <div class="col-sm-12 mb-3 mb-sm-0">
-                        <label> Selecteer bocht</label>
+                        <label> Selecteer bocht (1 tot 14)</label>
                         <input type="number" name="bocht_nr" class="form-control" placeholder=""
                             value="<?php  echo @$bocht_nr;?>">
-
                     </div>
                 </div>
             </tr>
@@ -71,9 +57,10 @@ if(isset($save))
             <tr>
                 <div class="form-group row input-group">
                     <div class="col-sm-12 mb-3 mb-sm-0">
-                        <label>Maximale snelheid om de bocht te nemen, in kilometer per uur</label>
+                        <label>Maximale snelheid om de bocht te nemen</label>
                         <input type="number" placeholder="" name="res" class="form-control" readonly="readonly"
                             disabled="disabled" value="<?php  echo @$max_snelheid;?>" />
+                        <label>in kilometer per uur</label>
                     </div>
                 </div>
 
